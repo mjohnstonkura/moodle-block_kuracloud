5d90d36ec76ef55c45789feb81a939817cce2a0d
t-neumegen <t.neumegen@adinstruments.com>
2017-09-22 11:52:37 +1200

    Updated version to todays date

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2017081000;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2017092200;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2016052300;        // Requires this Moodle version.
 $plugin->component = 'block_kuracloud'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE;
